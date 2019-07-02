bcbfdc948c18be9ff0abba9d5d55f9901eb8df25
Diego Augusto dos Santos <dsdiegods01@gmail.com>
2019-07-02 16:03:38 -0300

    Registrar vilao no admin

--- catalogo_viloes/admin.py
+++ catalogo_viloes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from catalogo_viloes.models import Vilao  #Importa a class Vilao da models
 
 # Register your models here.
+
+admin.site.register(Vilao)  #Class Vilao da models
